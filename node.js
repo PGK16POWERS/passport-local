89882dca9658b1796ae3e358c30f35101152c2aa
programmer Kartier <progrmmerKartier@gmail.com>
2023-10-04 21:21:05 +0200

    server starter

--- node.js
+++ node.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const passport = require("passport");
 const app = express();
 
 
-
-
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.session({secret: "Secret_Key"}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 
