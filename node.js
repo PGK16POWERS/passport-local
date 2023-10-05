976710ed564192fb3440226edc3d91a19dff0ceb
programmer Kartier <progrmmerKartier@gmail.com>
2023-10-05 06:55:56 +0200

    server

--- node.js
+++ node.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const mongoUri = "mongodb+srv://Marjella:Marjella@diamondburg.lxfxn0d.mongodb.net/?retryWrites=true&w=majority"
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(session({secret:"BigKahuna",resave:false , saveUninitialized:false}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 
+mongoose.connect(mongoUri,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}) .then (()=> {
+    console.log("Marjella Ke Star")
+}) .catch((error)=> {
+    console.error("Error Experienced: ", error)
+})
 
 app.get("/",(req,res)=> {
     res.send("Dundiditagain")
