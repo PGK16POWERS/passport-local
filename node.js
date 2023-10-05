0856a23912a008e8d5c90604ae033ae29ead282d
programmer Kartier <progrmmerKartier@gmail.com>
2023-10-05 06:43:38 +0200

    node + server

--- node.js
+++ node.js
@@ -1,3 +1,19 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+
+
+
+
+app.get("/",(req,res)=> {
+    res.send("Dundiditagain")
+})
+
+
+
+
+
+
+app.listen(5000, ()=> {
+    console.log("Danko Supreme")
+})
