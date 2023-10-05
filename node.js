a07858b0563056bf07c3739f405697e13478db82
programmer Kartier <progrmmerKartier@gmail.com>
2023-10-05 06:13:57 +0200

    node

--- node.js
+++ node.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const passport = require("passport");
-const session = require('express-session');
+const session = require("express-session");
+const LocalStrategy = require('passport-local').Strategy;
 const app = express();
 
 
+// MIDDLEWARES
 app.use(express.static("public"))
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -14,6 +16,27 @@ app.use(express.session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+
+passport.use(new LocalStrategy((username, password, done) => {
+    const user = users.find((u) => u.username === username);
+    if (!user) {
+      return done(null, false, { message: 'Incorrect username.' });
+    }
+    if (user.password !== password) {
+      return done(null, false, { message: 'Incorrect password.' });
+    }
+    return done(null, user);
+  }));
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    const user = users.find((u) => u.id === id);
+    done(null, user);
+  });
 
 
 app.get("/", (req,res)=> {
